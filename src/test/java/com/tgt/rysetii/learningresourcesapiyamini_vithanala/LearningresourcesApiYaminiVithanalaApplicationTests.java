ed7483a63c268ddfc80cf70635084bdc15a8ada3
Yaminivithanala <Suryayamini2001@gmail.com>
2022-07-02 13:15:02 +0530

    Hello World added

---
+++ src/test/java/com/tgt/rysetii/learningresourcesapiyamini_vithanala/LearningresourcesApiYaminiVithanalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapiyamini_vithanala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiYaminiVithanalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
